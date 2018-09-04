88755e662bf992d4ee03552a11e2a9aa646a5ec7
Kevin Dao <kevindao2006@gmail.com>
2018-09-04 18:38:02 -0500

    Initial Commit

---
+++ wk1_flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wk1_flix
+//
+//  Created by Kevin Dao on 9/4/18.
+//  Copyright © 2018 Kevin Dao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
